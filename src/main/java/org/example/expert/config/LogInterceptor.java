ab328d0ecb5edc5ea860a5a4ac7bcc44c2989997
jun5-soodal <j01024749503@gmail.com>
2025-02-25 20:08:51 +0900

    feat(lv4):관리자 인터셉터 추가
    -AdminAuthInterceptor 관리자 권한 체크
    -LogInterceptor 관리자 요청 정보 로깅
    -JwtFilter 관리자 권한 체크 분리

---
+++ src/main/java/org/example/expert/config/LogInterceptor.java
@@ -0,0 +1,18 @@
+package org.example.expert.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("요청 URL: {} , 요청 시간: {}", request.getRequestURI(), LocalDateTime.now());
+        return true;
+    }
+}
